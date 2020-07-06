6f16002882666d74f92a8a5186021d5a38f64ed8
LABCAT <shane.watters.nz@gmail.com>
2020-07-06 14:34:24 +1200

    MAJOR: upgrade to new version of Silverstripe - step: Add the table name

--- code/model/CustomProductList.php
+++ code/model/CustomProductList.php
@@ -25,6 +25,20 @@ class CustomProductList extends DataObject
      */
     private static $separator_alternative = ';';
 
+
+/**
+  * ### @@@@ START REPLACEMENT @@@@ ###
+  * OLD: private static $db (case sensitive)
+  * NEW: 
+    private static $table_name = '[SEARCH_REPLACE_CLASS_NAME_GOES_HERE]';
+
+    private static $db (COMPLEX)
+  * EXP: Check that is class indeed extends DataObject and that it is not a data-extension!
+  * ### @@@@ STOP REPLACEMENT @@@@ ###
+  */
+    
+    private static $table_name = 'CustomProductList';
+
     private static $db = array(
         "Title" => "Varchar(255)",
         "Locked" => "Boolean",
